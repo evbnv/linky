2bac04f8e3758a74a3a380c54c2b107976ad83a9
Evgeny Buyanov <kesha.lawrov@yandex.ru>
2025-09-20 22:59:36 +0300

    feat: add json response with short url

--- handlers/handler.go
+++ handlers/handler.go
@@ -11,6 +11,9 @@ import (
 type ShortenRequest struct {
 	URL string `json:"url"`
 }
+type ShortenResponse struct {
+	ShortURL string `json:"short_url"`
+}
 
 func HandleAddress(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -18,6 +21,8 @@ func HandleAddress(w http.ResponseWriter, r *http.Request) {
 
 	case "POST":
 		var req ShortenRequest
+		var resp ShortenResponse
+		var shortUrl string
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -31,7 +36,7 @@ func HandleAddress(w http.ResponseWriter, r *http.Request) {
 		longUrl := req.URL
 
 		for {
-			shortUrl := store.URLTransform(longUrl)
+			shortUrl = store.URLTransform(longUrl)
 
 			if _, ok := store.URLs[shortUrl]; !ok {
 				store.URLs[shortUrl] = longUrl
@@ -41,10 +46,15 @@ func HandleAddress(w http.ResponseWriter, r *http.Request) {
 			longUrl = longUrl + ":" + strconv.Itoa(store.Count)
 			store.Count++
 		}
-
+		//
 		fmt.Println(store.URLs)
+		//
 
+		resp = ShortenResponse{ShortURL: shortUrl}
+
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(resp)
 	default:
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
